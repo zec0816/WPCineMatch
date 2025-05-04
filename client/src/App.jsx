1c8f6363c9f322599eeb2e3243d2f12fb5353120
rj <rjy0713@gmail.com>
2025-05-04 18:05:00 +0800

    roujun search bar on 4 May

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,28 @@
-import { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Signup from './Signup'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './Login'
-import Home from './Home'
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Signup from "./Signup";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Home from "./Home";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='register' element={<Signup />}> </Route>
-        <Route path='login' element={<Login />}> </Route>
-        <Route path='home' element={<Home />}> </Route>
+        <Route path="register" element={<Signup />}>
+          {" "}
+        </Route>
+        <Route path="login" element={<Login />}>
+          {" "}
+        </Route>
+        <Route path="home" element={<Home />}>
+          {" "}
+        </Route>
       </Routes>
     </BrowserRouter>
-
-  )
+  );
 }
 
-export default App
+export default App;
